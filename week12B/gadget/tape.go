df84a969e9504a2d0f8961142e041cadea469913
hwangjk95 <hwangje95@gmail.com>
2022-11-18 15:24:53 +0900

    week12

---
+++ week12B/gadget/tape.go
@@ -0,0 +1,31 @@
+package gadget
+
+import "fmt"
+
+type TapePlayer struct {
+	Batteries string
+}
+
+type TapeRecorder struct {
+	Microphones int
+}
+
+func (t TapePlayer) Play(song string) {
+	fmt.Println("Playing", song)
+}
+
+func (t TapePlayer) Stop() {
+	fmt.Println("Stopped!")
+}
+
+func (t TapeRecorder) Play(song string) {
+	fmt.Println("Playing", song)
+}
+
+func (t TapeRecorder) Stop() {
+	fmt.Println("Stopped!")
+}
+
+func (t TapeRecorder) Record() {
+	fmt.Println("Recording")
+}
---
+++ week12B/play.go
@@ -0,0 +1,43 @@
+// package main
+
+// import "gadget"
+
+// func playList(device gadget.TapePlayer, songs []string) {
+// 	for _, song := range songs {
+// 		device.Play(song)
+// 	}
+// 	device.Stop()
+// }
+
+// func main() {
+// 	player := gadget.TapePlayer{}
+// 	mixtape := []string{"좋은날", "너랑나", "투명우산"}
+// 	playList(player, mixtape)
+
+// }
+
+package main
+
+import "gadget"
+
+type Player interface {
+	Play(string)
+	Stop()
+}
+
+func playList(device Player, songs []string) {
+	for _, song := range songs {
+		device.Play(song)
+	}
+	device.Stop()
+}
+
+func main() {
+	player := gadget.TapePlayer{}
+	mcrtape := []string{"혜성", "비밀번호486", "별의조각"}
+	mixtape := []string{"좋은날", "너랑나", "투명우산"}
+	playList(player, mixtape)
+	playList(player, mcrtape)
+}
+
+//추상클래스와 인터페이스 개념(시험x 개념중요)
